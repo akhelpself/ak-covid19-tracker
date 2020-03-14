b3422fbeb0c2d5dc3cdae24495907e77dedb44ed
Andy <noname@gmail.com>
2020-03-14 15:36:50 -0600

    update symptoms page

--- Covid19_Tracker/app/src/main/java/com/akdev/covid19tracker/ui/notifications/SymptomsFragment.java
+++ Covid19_Tracker/app/src/main/java/com/akdev/covid19tracker/ui/notifications/SymptomsFragment.java
@@ -14,17 +14,17 @@ import androidx.lifecycle.ViewModelProviders;
 
 import com.akdev.covid19tracker.R;
 
-public class NotificationsFragment extends Fragment {
+public class SymptomsFragment extends Fragment {
 
-    private NotificationsViewModel notificationsViewModel;
+    private SymptomsViewModel symptomsViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        notificationsViewModel =
-                ViewModelProviders.of(this).get(NotificationsViewModel.class);
+        symptomsViewModel =
+                ViewModelProviders.of(this).get(SymptomsViewModel.class);
         View root = inflater.inflate(R.layout.fragment_notifications, container, false);
         final TextView textView = root.findViewById(R.id.text_notifications);
-        notificationsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        symptomsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
--- Covid19_Tracker/app/src/main/java/com/akdev/covid19tracker/ui/notifications/SymptomsViewModel.java
+++ Covid19_Tracker/app/src/main/java/com/akdev/covid19tracker/ui/notifications/SymptomsViewModel.java
@@ -4,11 +4,11 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class NotificationsViewModel extends ViewModel {
+public class SymptomsViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
 
-    public NotificationsViewModel() {
+    public SymptomsViewModel() {
         mText = new MutableLiveData<>();
         mText.setValue("This is notifications fragment");
     }
