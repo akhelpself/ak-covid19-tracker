eb28c552f0b73bebae151344779572a9f0feff8e
Andy <noname@gmail.com>
2020-03-17 23:29:13 -0600

    update timeline title

--- covid19-backend/src/main/java/com/akdev/covid19/controllers/TimelineController.java
+++ covid19-backend/src/main/java/com/akdev/covid19/controllers/TimelineController.java
@@ -6,6 +6,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import static com.akdev.covid19.utils.Constant.KEY_HEADER_TITLE;
+
 @Controller
 @RequestMapping(value = "timeline")
 public class TimelineController {
@@ -19,6 +21,7 @@ public class TimelineController {
     @GetMapping
     public String timeline(Model model) throws Exception {
         model.addAttribute("timeline", timelineParsing.getData());
+        model.addAttribute(KEY_HEADER_TITLE, "Covid-19 Timeline for major updates (GMT)");
         return "timeline";
     }
 }
